6e502b9871fb3e1c50aec3d8af7888d833d742dc
Hazmat <ebertrand@gaming.tech>
2021-11-18 18:33:13 +0100

    les projets fonctionnels

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ $(document).ready(function(){
  $(document).ready(function(){
     $('.carousel').carousel();
   });
+
+$(easter).on('click','body *',function(){
+  .css-selector {
+    background: linear-gradient(270deg, #26a69a, #311b92, #000000);
+    background-size: 600% 600%;
+
+    -webkit-animation: AnimationName 15s ease infinite;
+    -moz-animation: AnimationName 15s ease infinite;
+    animation: AnimationName 15s ease infinite;
+}
+
+@-webkit-keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@-moz-keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
